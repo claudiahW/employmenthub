e5ab6d7657a3122bd5087f8575017f71d8e0ffb5
claudiahW <murungiclaud@gmail.com>
2024-03-23 21:01:55 +0300

    add a key prop to the list elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
 const App = () => {
+  const name = 'John';
+const x = 10;
+const y = 20;
+const names = ['Brad', 'Mary', 'Joe', 'Sarah'];
+
   return (
-    <div>App</div>
+    <>
+    <div className='text-5xl'>App</div>
+    <p>Hello {name}</p>
+    <p>
+      The sum of {x} and {y} is {x + y} 
+    </p>
+    <ul>
+      { names.map((name, index) => (
+        <li key={index}>{ name }</li>
+      )) }
+    </ul>
+    </>
   )
 }
 
